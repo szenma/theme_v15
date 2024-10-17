7bf77f1f55a8d74c3cd7088514c4a11e8b6b0275
sippy <szenma@gmail.com>
2024-10-17 10:10:56 +0530

    Add files via upload

---
+++ theme_v15/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Theme V15",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Business Theme V14")
+		}
+	]
