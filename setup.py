a828a1b79a6c08ef28b7242639c1115aa539fd86
sippy <szenma@gmail.com>
2024-10-17 12:50:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in business_theme_v14/__init__.py
-from btheme_v15 import __version__ as version
+from theme_v15 import __version__ as version
 
 setup(
 	name="theme_v15",
