cdadb0e3e3f33cb6db0ded488047c1082e3385d2
sippy <szenma@gmail.com>
2024-10-17 12:46:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in business_theme_v14/__init__.py
-from business_theme_v14 import __version__ as version
+from btheme_v15 import __version__ as version
 
 setup(
-	name="business_theme_v14",
+	name="theme_v15",
 	version=version,
-	description="Business Theme for ERPNext / Frappe",
-	author="Midocean Technologies Pvt Ltd",
-	author_email="sagar@midocean.tech",
+	description=" Theme for ERPNext / Frappe",
+	author="Mszenma",
+	author_email="szenma@gmail.com",
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
